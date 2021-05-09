fb5460ae514a58561e63495b7e475dd70c524cf1
Evgenii <e.galimsarov@gmail.com>
2021-05-09 15:32:03 +0300

    Adding Index Page and controller. closes #8.

---
+++ src/main/kotlin/galimsarov/springframework/petclinic/controllers/IndexController.kt
@@ -0,0 +1,10 @@
+package galimsarov.springframework.petclinic.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class IndexController {
+    @RequestMapping("", "/", "index", "index.html")
+    fun index() = "index"
+}
