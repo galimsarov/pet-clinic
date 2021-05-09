1bbdd640fb2e8624fc4658464e2bee20a5f3724a
Evgenii <e.galimsarov@gmail.com>
2021-05-09 19:45:29 +0300

    Adding vets page and controller. closes #7.

---
+++ src/main/kotlin/galimsarov/springframework/petclinic/controllers/VetController.kt
@@ -0,0 +1,10 @@
+package galimsarov.springframework.petclinic.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class VetController {
+    @RequestMapping("/vets", "/vets/index", "/vets/index.html")
+    fun listVets() = "vets/index"
+}
