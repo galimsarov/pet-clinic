d1112fd013e1abe698ac0e5f9e1284adae08ecb0
Evgenii <e.galimsarov@gmail.com>
2021-05-12 12:53:15 +0300

    Added vet list and updated controller. Closes #15

--- src/main/kotlin/galimsarov/springframework/petclinic/controllers/VetController.kt
+++ src/main/kotlin/galimsarov/springframework/petclinic/controllers/VetController.kt
@@ -1,10 +1,15 @@
 package galimsarov.springframework.petclinic.controllers
 
+import galimsarov.springframework.petclinic.services.VetService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class VetController {
+class VetController(private val vetService: VetService) {
     @RequestMapping("/vets", "/vets/index", "/vets/index.html")
-    fun listVets() = "vets/index"
+    fun listVets(model: Model): String {
+        model.addAttribute("vets", vetService.findAll())
+        return "vets/index"
+    }
 }
