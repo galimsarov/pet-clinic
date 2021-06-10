1e8a7ac9e25052e5cc842c19fa3a48a61ea84700
e.galimsarov <e.galimsarov@gmail.com>
2021-06-10 09:30:44 +0500

    Updated base entity with JPA annotations. Closes #35

--- src/main/kotlin/galimsarov/springframework/petclinic/model/BaseEntity.kt
+++ src/main/kotlin/galimsarov/springframework/petclinic/model/BaseEntity.kt
@@ -1,7 +1,14 @@
 package galimsarov.springframework.petclinic.model
 
 import java.io.Serializable
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 
+@MappedSuperclass
 open class BaseEntity: Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0
 }
